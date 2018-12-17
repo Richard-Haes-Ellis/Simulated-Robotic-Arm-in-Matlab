47439526f095b386211cf83d8a4eb42921b49b73
richaeell <richaeell@alum.us.es>
2018-12-17 22:18:46 +0100

    Added compensator option with reference signals or measured signals

--- Control/A_DisenoControladores.m
+++ Control/A_DisenoControladores.m
@@ -103,7 +103,36 @@ code = ['function senalControl = Controller(in)\n\n'...
 'qppr = [in(13); in(14); in(15)];\n\n'... 
 't = in(16);\n\n'];
 fprintf(file,code);
-
+comps = questdlg('¿Añadir Compensador?','Diseño','Si','No','No');
+switch comps
+    case 'Si'
+        answer = questdlg('¿Modelo real o ideal?','Diseño','Ideal','Real','Ideal');
+        switch answer
+            case 'Ideal'
+                load('modeloIdeal.mat');
+            case 'Real'
+                load('modeloReal.mat');
+        end
+        list = {'Compensador con q medida',...
+                'Compensador con q de referencia',...
+                'Compensador dynamico con q medida',...
+                'Compensador dynamico con q referencia'};
+        [indx,tf] = listdlg('PromptString','Selecionar Compensador','SelectionMode','single','ListSize',[350,70],'ListString',list);
+    switch indx
+        case 1
+            % Compensador estatico con medidas
+            fprintf(file,'q1 = q(1); q2 = q(2); q3 = q(3);\n');
+        case 2
+            % Compensador estatico con referencias
+            fprintf(file,'q1 = qr(1); qr2 = q(2); qr3 = q(3);\n');
+        case 3
+            % Compensador dinamico con medidas
+        case 4
+            % Compensador dinamico con referencias
+    end
+    fprintf(file,'Ga = [%s;\n%s;\n%s];\n\n',char(G_num(1)),char(G_num(2)),char(G_num(3)));
+    case 'No'
+end
 fprintf(file,'Kp = diag([%f\t%f\t%f]);\n',Kp1,Kp2,Kp3);
 fprintf(file,'Ki = diag([Kp(1,1)/%f\tKp(2,2)/%f\tKp(3,3)/%f]);\n',Ti1,Ti2,Ti3);
 fprintf(file,'Kd = diag([Kp(1,1)*%f\tKp(2,2)*%f\tKp(3,3)*%f]);\n\n',Td1,Td2,Td3);
@@ -122,9 +151,16 @@ code = ['persistent ek_i ek_1\n' ...
 'epk = qpr-qp;\n\n' ...
 '%% Calculo del error integral (Parte integral) (Metodo trapezoidal)\n' ...
 'ek_i = ek_i + (Ts/2)*(ek+ek_1);\n\n' ...
-'%% Cálculo de la señal de control incremental generada por el controlador C(z)\n' ...
-'Imk = Kp*ek + Kd*epk + Ki*ek_i;\n\n' ...
-'%% Actualizamos error del instante k-1\n' ...
+'%% Cálculo de la señal de control incremental generada por el controlador C(z)\n'];
+fprintf(file,code);
+switch comps
+    case 'Si'
+        fprintf(file,'Imk = Kp*ek + Kd*epk + Ki*ek_i + Ga;\n\n');
+    case 'No'
+        fprintf(file,'Imk = Kp*ek + Kd*epk + Ki*ek_i;\n\n');
+end
+
+code = ['%% Actualizamos error del instante k-1\n' ...
 'ek_1 = ek;\n\n' ...
 'senalControl = Imk;\n' ...
 'disp(t);\n' ...
