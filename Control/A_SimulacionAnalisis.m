6fee205f1f866fccc9fc1f9227eaa5bc3ccfaec9
richaeell <richaeell@alum.us.es>
2018-12-18 17:02:09 +0100

    Added labels and legends to plots and the error in cartesian space

--- Control/A_SimulacionAnalisis.m
+++ Control/A_SimulacionAnalisis.m
@@ -27,6 +27,7 @@ set(plt(3),'DisplayName','$q_1$ ref. muestreado');
 leg = legend(splt,'show');
 set(leg,'Interpreter','latex');
 ylabel('rad');
+xlabel('time');
 grid;
 % Plot q2 vs qr2
 splt = subplot(3,2,3);
@@ -37,6 +38,7 @@ set(plt(3),'DisplayName','$q_2$ ref. muestreado');
 leg = legend(splt,'show');
 set(leg,'Interpreter','latex');
 ylabel('rad');
+xlabel('time');
 grid;
 % Plot q3 vs qr3
 splt = subplot(3,2,5);
@@ -47,22 +49,35 @@ set(plt(3),'DisplayName','$q_3$ ref. muestreado');
 leg = legend(splt,'show');
 set(leg,'Interpreter','latex');
 ylabel('rad');
+xlabel('time');
 grid;
 
 % Error q1
-subplot(3,2,2);
-plot(t,q(:,1)-qrs(:,1));
-ylabel('Error q1');
+splt = subplot(3,2,2);
+plt = plot(t,q(:,1)-qrs(:,1));
+set(plt(1),'DisplayName','Error de $q_1$');
+leg = legend(splt,'show');
+set(leg,'Interpreter','latex');
+ylabel('rad');
+xlabel('time');
 grid;
 % Error q2
-subplot(3,2,4);
-plot(t,q(:,2)-qrs(:,2));
-ylabel('Error q2');
+splt = subplot(3,2,4);
+plt = plot(t,q(:,2)-qrs(:,2));
+set(plt(1),'DisplayName','Error de $q_2$');
+leg = legend(splt,'show');
+set(leg,'Interpreter','latex');
+ylabel('rad');
+xlabel('time');
 grid;
 % Error q3
-subplot(3,2,6);
-plot(t,q(:,3)-qrs(:,3));
-ylabel('Error q3');
+splt = subplot(3,2,6);
+plt = plot(t,q(:,3)-qrs(:,3));
+set(plt(1),'DisplayName','Error de $q_3$');
+leg = legend(splt,'show');
+set(leg,'Interpreter','latex');
+ylabel('rad');
+xlabel('time');
 grid;
 
 figure('units','normalized','outerposition',[0 0 1 1])
@@ -75,6 +90,7 @@ set(plt(3),'DisplayName','$\dot q_1$ ref. muestreado');
 leg = legend(splt,'show');
 set(leg,'Interpreter','latex');
 ylabel('rad/s');
+xlabel('time');
 grid;
 % Plot qd2 vs qdr2
 splt = subplot(3,2,3);
@@ -85,64 +101,104 @@ set(plt(3),'DisplayName','$\dot q_2$ ref. muestreado');
 leg = legend(splt,'show');
 set(leg,'Interpreter','latex');
 ylabel('rad/s');
+xlabel('time');
 grid;
 % Plot qd3 vs qdr3
 splt = subplot(3,2,5);
 plt  = plot(t,qd(:,3),'r',t,qdr(:,3),'b',t,qdrs(:,3),'k');
-set(plt(1),'DisplayName','$\dot q_3$ ','Color',[1 0 0]);
+set(plt(1),'DisplayName','$\dot q_3$ ');
 set(plt(2),'DisplayName','$\dot q_3$ referencia');
 set(plt(3),'DisplayName','$\dot q_3$ ref. muestreado');
 leg = legend(splt,'show');
 set(leg,'Interpreter','latex');
 ylabel('rad/s');
+xlabel('time');
 grid;
 
 % Error qd1
-subplot(3,2,2);
-plot(t,qd(:,1)-qdrs(:,1));
-ylabel('Error qd1');
+splt = subplot(3,2,2);
+plt = plot(t,qd(:,1)-qdrs(:,1));
+set(plt(1),'DisplayName','Error de $\dot q_1$ ');
+leg = legend(splt,'show');
+set(leg,'Interpreter','latex');
+ylabel('rad/s');
+xlabel('time');
 grid;
 % Error qd2
-subplot(3,2,4);
-plot(t,qd(:,2)-qdrs(:,2));
-ylabel('Error qd2');
+splt = subplot(3,2,4);
+plt = plot(t,qd(:,2)-qdrs(:,2));
+set(plt(1),'DisplayName','Error de $\dot q_2$ ');
+leg = legend(splt,'show');
+set(leg,'Interpreter','latex');
+ylabel('rad/s');
+xlabel('time');
 grid;
 % Error qd3
-subplot(3,2,6);
-plot(t,qd(:,3)-qdrs(:,3));
-ylabel('Error qd3');
+splt = subplot(3,2,6);
+plt = plot(t,qd(:,3)-qdrs(:,3));
+set(plt(1),'DisplayName','Error de $\dot q_3$ ');
+leg = legend(splt,'show');
+set(leg,'Interpreter','latex');
+ylabel('rad/s');
+xlabel('time');
 grid;
-
 
 figure('units','normalized','outerposition',[0 0 1 1])
 
 % Corriente Im1
-subplot(3,2,1);
-plot(t(1:end-3),Im(:,1));
-ylabel('Corriente Im2');
+splt = subplot(3,2,1);
+plt = plot(t(1:end-3),Im(:,1));
+set(plt(1),'DisplayName','Corriente $Im_1$');
+leg = legend(splt,'show');
+set(leg,'Interpreter','latex');
+ylabel('Amperaje');
+xlabel('time');
 grid;
 % Corriente Im2
-subplot(3,2,3);
-plot(t(1:end-3),Im(:,2));
-ylabel('Corriente Im2');
+splt = subplot(3,2,3);
+plt = plot(t(1:end-3),Im(:,2));
+set(plt(1),'DisplayName','Corriente $Im_2$');
+leg = legend(splt,'show');
+set(leg,'Interpreter','latex');
+ylabel('Amperaje');
+xlabel('time');
 grid;
 % Corriente Im3
-subplot(3,2,5);
-plot(t(1:end-3),Im(:,3));
-ylabel('Corriente Im3');
+splt = subplot(3,2,5);
+plt = plot(t(1:end-3),Im(:,3));
+set(plt(1),'DisplayName','Corriente $Im_3$');
+leg = legend(splt,'show');
+set(leg,'Interpreter','latex');
+ylabel('Amperaje');
+xlabel('time');
 grid;
 
 % Trayectoria de referencia vs medida
-subplot(3,2,[2 4 6]);
-plot(xyz(:,2),xyz(:,3),'r',xyzr(:,2),xyzr(:,3),'b');
+splt = subplot(3,2,[2 4]);
+plt  = plot(xyz(:,2),xyz(:,3),'r',xyzr(:,2),xyzr(:,3),'b');
+set(plt(1),'DisplayName','Trayectoria medida');
+set(plt(2),'DisplayName','Trayectoria referencia');
+leg = legend(splt,'show');
+set(leg,'Interpreter','latex');
 ylabel('Y');
 xlabel('Z');
 grid;
+
+splt = subplot(3,2,6);
+plt  = plot(t(1:end-3),vecnorm((xyz-xyzr)')','b');
+set(plt(1),'DisplayName','Error distancia');
+leg = legend(splt,'show');
+set(leg,'Interpreter','latex');
+ylabel('Metros');
+xlabel('time');
+grid;
+
+%% Guardar controlador
 
 answer = questdlg('¿Guardar Controlador?','Prompt','Si','No','Si');
 
 switch answer
     case 'Si'
-        uisave({'Crl1','Crl2','Crl3','Gs1','Gs2','Gs3'});
+        uisave({'Crl1','Crl2','Crl3','Gs1','Gs2','Gs3','M_num','V_num','G_num','tipoControl'});
     case 'No'
 end
